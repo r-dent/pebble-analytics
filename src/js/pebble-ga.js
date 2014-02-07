1615117da9c4f5688582669b18490e71d1ec64b4
Roman Gille <developer@romangille.com>
2014-02-07 01:43:55 +0100

    Structured code
    Signed-off-by: Roman Gille <developer@romangille.com>

--- src/js/pebble-ga.js
+++ src/js/pebble-ga.js
@@ -1,8 +1,3 @@
-/**
- *  Analytics code
- *  Imagine this would be imported from, let´s say, pebble-ga.js.
- */
-
 var Analytics = function(analyticsId, appName, appVersion) {
   this.analyticsId = analyticsId;
   this.appName = appName;
@@ -29,8 +24,6 @@ Analytics.prototype._trackGA = function(type, params) {
   req.open('POST', url, true);
   req.setRequestHeader('Content-length', trackingParams.length);
   req.send(trackingParams);
-
-  console.log('tracking '+ type);
 }
 
 Analytics.prototype.trackScreen = function (screenName) {
@@ -41,39 +34,5 @@ Analytics.prototype.trackEvent = function (category, action) {
   this._trackGA('event', {'ec': category, 'ea': action});
 }
 
-/**
- *  End of Analytics code
- *  Imagine this were imported from, let´s say, pebble-ga.js.
- */
-
-// If require() will work some day. This would be the way.
-// require('pebble-ga');
-
-var ga = new Analytics('UA-1234567-0', 'myApp', '1.0.0');
-
-var messageType = {
-  "track_screen" : 0,
-  "track_event" : 1
-}
-
-Pebble.addEventListener('ready', function(e) {
-  console.log('JavaScript app ready and running!');
-
-  ga.trackEvent('lifecycle', 'start App');
-});
-
-Pebble.addEventListener('appmessage', function(e) {
-  if (e.payload['message_type'] !== undefined) {
-    switch(e.payload['message_type']) {
-
-      case messageType['track_screen']:
-        ga.trackScreen(e.payload['track_screen_name']);
-        break;
-
-      case messageType['track_event']:
-        ga.trackEvent(e.payload['track_event_category'], e.payload['track_event_action']);
-        break;
-
-    }
-  } 
-});
+// If require() will work some day. This would be nessecary.
+// module.exports.Analytics = Analytics;
